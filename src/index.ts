e336eeb6d845ca4b27fc8e7c1fab5e3e35fa8a1d
so1ve <i@mk1.io>
2023-05-04 21:39:58 +0800

    fix: export

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function onError(err: any) {
   throw err; /* ↓ Check stack trace ↓ */
 }
 
-module.exports = function (filename: string, opts: KazuyaOptions) {
+export default (filename: string, opts: KazuyaOptions) => {
   opts = { onError, ...opts };
 
   return kazuya(filename, opts);
